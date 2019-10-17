172968ac5c3c5aac7423541f58be40bc360c1e5b
ShallowDock <741617502@qq.com>
2019-10-17 22:37:26 +0800

    finish assignment3

---
+++ assignment3/Task.py
@@ -0,0 +1,114 @@
+#%%
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+import torch.optim as optim
+
+#%% 
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+#%%
+transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data/cifar10', train=True, download=False, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,shuffle=True, num_workers=0)
+testset = torchvision.datasets.CIFAR10(root='./data/cifar10', train=False, download=False, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4,shuffle=False, num_workers=0)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+#%%
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+def testNet(testloader, net):
+    running_loss = 0.0
+    for i, (inputs, labels) in enumerate(testloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = inputs.to(device), labels.to(device)    
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        running_loss += loss.item()
+    print('total loss on test set: {0}'.format(running_loss/i))
+
+# get some random training images
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+
+# show images
+imshow(torchvision.utils.make_grid(images))
+# print labels
+print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+#%% get cuda ready for training
+# but GPU usage is just 10% or so...observed by nvidia inspector
+useCuda = torch.cuda.is_available()
+device = torch.device("cuda" if useCuda else "cpu")
+
+#%%
+# initialize neural net, setup loss and optimizer
+net = Net().to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+#%%
+for epoch in range(3):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, (inputs, labels) in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = inputs.to(device), labels.to(device)
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
+
+
+PATH = 'assignment3/cifar_net.pth'
+torch.save(net.state_dict(), PATH)
+
+#%%
+dataiter = iter(testloader)
+images, labels = dataiter.next()
+# print images
+imshow(torchvision.utils.make_grid(images))
+print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+testNet(testloader, net)
+#%%
